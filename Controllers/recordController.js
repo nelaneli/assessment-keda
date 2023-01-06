6f3cd2cf209c744dc74be0ce04cb1d75a4f1f0b4
Yuna Sugianela <yuna.sugianela@bumi-labs.com>
2023-01-06 10:07:01 +0700

    feat(api): count total amount

--- Controllers/recordController.js
+++ Controllers/recordController.js
@@ -22,25 +22,84 @@ const createRecord = async (req, res) => {
 
 const endRecord = async (req, res) => {
   // count total amount
+  let get_record
   try {
-      const record = await Record.update(
-        {
-          end_time: moment(),
-          total_amount: 0
+    get_record = await Record.findOne(
+      {
+        where: {
+          vehicle_no: req.body.vehicle_no,
+          end_time: null
         },
-        {
-          where: {
-            vehicle_no,
-            end_time: null
-          }
+        raw: true
+      }
+    )
+  } catch (error) {
+    return res.send(error)
+  }
+  console.log(get_record)
+
+  let end_time = moment()
+  let duration = moment.duration(end_time.diff(get_record.start_time));
+
+  //Get Minutes
+  let total_minutes = duration.asMinutes();
+  console.log(total_minutes)
+
+  // minute to hour
+  let hours = Math.floor(total_minutes / 60);
+  let minutes = total_minutes % 60;
+  console.log(hours, minutes)
+
+  // hour to day
+  let days = Math.floor(hours / 24);
+  hours = hours % 24;
+  console.log(days, hours)
+
+  let type = get_record.type;
+  if(type != 'mobil' && type != 'motor'){
+    return res.status(400).json({
+      message: 'type harus mobil atau motor'
+    })
+  }
+
+  let amount_per_hour = type == 'mobil' ? 5000 : 2000;
+  let max_amount_per_day = type == 'mobil' ? 80000 : 40000;
+
+  // count amount for hour, jika lebih dari maximum harian, maka dijadikan nilai max
+  let total_amount_hour = hours * amount_per_hour;
+  total_amount_hour = total_amount_hour >= (days * max_amount_per_day) ? (days * max_amount_per_day) : total_amount_hour;
+
+  // count amount for days
+  let total_amount_day = days * max_amount_per_day;
+
+  // count amount for minutes
+  let total_amount_minutes = 0
+  if(minutes > 0){
+    total_amount_minutes = amount_per_hour
+  }
+
+  // count total amount
+  let total_amount = total_amount_day + total_amount_hour + total_amount_minutes
+
+  try {
+    const record = await Record.update(
+      {
+        end_time: moment(),
+        total_amount,
+      },
+      {
+        where: {
+          id: get_record.id
         }
-      );
-      return res.status(200).send(record)
-    } catch (error) {
-      return res.status(400).send(error);
-    }
+      }
+    );
+    return res.status(200).send(record)
+  } catch (error) {
+    return res.status(400).send(error);
+  }
 }
 
 module.exports = {
-    createRecord
+    createRecord,
+    endRecord
 }
--- Routes/recordRoutes.js
+++ Routes/recordRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const recordController = require('../Controllers/recordController')
-const { createRecord } = recordController
+const { createRecord, endRecord } = recordController
 
 const router = express.Router()
 
 router.post('/', createRecord)
+router.post('/end', endRecord)
 
 module.exports = router
